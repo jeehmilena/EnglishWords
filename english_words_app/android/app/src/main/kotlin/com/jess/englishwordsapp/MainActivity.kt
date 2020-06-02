6bac800d1682e5cc323a1cf5402fbf6341d50ed0
jeehmilena <jessicamilenacorrea@gmail.com>
2020-06-02 12:02:35 -0300

    first commit

---
+++ english_words_app/android/app/src/main/kotlin/com/jess/englishwordsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jess.englishwordsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ english_words_app/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RandomWords(title: 'English words'),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  RandomWords({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = 1 ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
